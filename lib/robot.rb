5cebca09cd8679fb890d74f94286a2e7a540afd1
Umer Dar <umerbashirdar@gmail.com>
2015-04-19 01:07:06 -0500

    more testing

--- lib/robot.rb
+++ lib/robot.rb
@@ -2,6 +2,8 @@ class Robot
 	def sayhello
 		"hi"
 	end
-
+	def add(number1, number2)
+		number1 + number2
+	end
 
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -6,4 +6,9 @@ describe 'robot' do
 		greeting = robot.sayhello
 		expect(greeting).to eq 'hi'
 	end
+	it 'should add two numbers' do  
+		robot = Robot.new
+		result = robot.add 2, 2
+		expect(result).to eq 4
+	end
 end
